d11ccc7a94ceb0f3f24f5264db7cf8941d1cc305
Monika <monika.dybalska@hotmail.com>
2024-04-08 16:57:39 +0200

    feat: add WP taxonomies and align code

--- app/page.tsx
+++ app/page.tsx
@@ -4,107 +4,25 @@ import { color } from "@material-tailwind/react/types/components/alert";
 import PricingPreview from "./components/pricing/pricing-packages-preview";
 import Section from "./components/homepage/section";
 import { getHomepagePreviews } from "@/lib/api";
-import { PreviewCategories, PreviewSubsection } from "../lib/types";
+import { HomepageSection } from "../lib/types";
 
 export default async function Home({ params }: { params: { slug: string } }) {
-  const cards: PreviewCategories | null = await getHomepagePreviews();
-  let subsectionData: PreviewSubsection[] | null = null;
-  if (cards) {
-    subsectionData = [
-      {
-        title: ["Visit the ", "monumental sights"],
-        color: "yellow",
-        cards: cards.sights.map((card) => {
-          return {
-            title: card.title,
-            image: card.image.node.mediaItemUrl,
-            description: card.preview.description,
-          };
-        }),
-        cta: "See more sights",
-      },
-      {
-        title: ["Discover the ", "nature"],
-        color: "yellow",
-        cards: new Array(6).fill(
-          cards.nature.map((card) => {
-            return {
-              title: card.title,
-              image: card.image.node.mediaItemUrl,
-              description: card.preview.description,
-            };
-          })[0]
-        ),
-        cta: "See more trails",
-      },
-      {
-        title: ["Taste the ", "cuisine"],
-        color: "yellow",
-        cards: new Array(6).fill(
-          cards.food.map((card) => {
-            return {
-              title: card.title,
-              image: card.image.node.mediaItemUrl,
-              description: card.preview.description,
-            };
-          })[0]
-        ),
-        cta: "See more restaurants",
-      },
-      {
-        title: ["Book your ", "perfect stay"],
-        color: "yellow",
-        cards: new Array(6).fill(
-          cards.accommodation.map((card) => {
-            return {
-              title: card.title,
-              image: card.image.node.mediaItemUrl,
-              description: card.preview.description,
-            };
-          })[0]
-        ),
-        cta: "See more accommodations",
-      },
-    ];
-  }
+  const sections: HomepageSection[] | null = await getHomepagePreviews();
 
   return (
     <main className="w-full border border-transparent">
       <HomepageHero />
-      {subsectionData && (
-        <Section
-          label="See and do"
-          color="yellow"
-          subsections={subsectionData}
-        />
-      )}
-      {/* {cardsContent.map(
-        (
-          section: {
-            label: string;
-            color: color | undefined;
-            subsections: {
-              title: string[];
-              cards: {
-                title: string;
-                image?: string;
-                description: string;
-              }[];
-              cta: string;
-            }[];
-          },
-          i
-        ) => {
+      {sections &&
+        sections.map((section: HomepageSection, i) => {
           return (
             <Section
-              label={section.label}
+              title={section.title}
               color={section.color}
               subsections={section.subsections}
               key={i}
             />
           );
-        }
-      )} */}
+        })}
       <PricingPreview />
     </main>
   );
--- lib/types.ts
+++ lib/types.ts
@@ -1,53 +1,62 @@
 import { color } from "@material-tailwind/react/types/components/alert";
 
-export interface PreviewNode {
-  title: string;
-  placeFields: {
-    category: string[];
-    image: {
-      node: {
-        mediaItemUrl: string;
-      };
-    };
-    preview: {
-      description: string;
-      cta: string;
-      displayOnHomepage: boolean;
-      priority: number;
-    };
+export interface ImageUrl {
+  node: {
+    mediaItemUrl: string;
   };
+}
+
+export interface HomepageData {
+  sections: {
+    nodes: {
+      name: HomepageSection["title"];
+      sectionFields: {
+        order: HomepageSection["order"];
+        color: color;
+      };
+      children: {
+        nodes: {
+          name: HomepageSubsection["title"];
+          subsectionFields: {
+            order: HomepageSubsection["order"];
+            title: string;
+            cta: string;
+          };
+          places: {
+            nodes: {
+              placeFields: {
+                preview: Preview;
+              };
+            }[];
+          };
+        }[];
+      };
+    }[];
+  };
+}
+
+export interface HomepageSection {
+  order: number;
+  title: string;
+  color: color;
+  subsections: HomepageSubsection[];
+}
+
+export interface HomepageSubsection {
+  order: number;
+  name?: string;
+  title: string;
+  cta: string;
+  places: Preview[];
+  color?: color;
 }
 
 export interface Preview {
-  category: string[];
-  image: {
-    node: {
-      mediaItemUrl: string;
-    };
-  };
-  preview: {
-    description: string;
-    cta: string;
-    displayOnHomepage: boolean;
-    priority: number;
-  };
   title: string;
-}
-
-export interface PreviewCategories {
-  sights: Preview[];
-  nature: Preview[];
-  food: Preview[];
-  accommodation: Preview[];
-}
-
-export interface PreviewSubsection {
-  title: string[];
-  color: color | undefined;
-  cards: {
-    title: string;
-    image?: string;
-    description: string;
-  }[];
+  thumbnail: ImageUrl;
+  description: string;
   cta: string;
+  displayOnHomepage?: boolean;
+  priority: number;
+  color?: color;
 }
