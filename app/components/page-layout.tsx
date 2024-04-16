8b947916c0867e7255e23413706e92ac148b5fbf
Monika <monika.dybalska@hotmail.com>
2024-04-16 11:20:35 +0200

    refactor: create a layout component to use for all subcategory pages

---
+++ app/components/page-layout.tsx
@@ -0,0 +1,13 @@
+export default function PageLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="flex flex-col items-center justify-between px-5 md:px-20">
+      <div className="flex flex-col w-full max-w-screen-xl gap-16">
+        {children}
+      </div>
+    </div>
+  );
+}
