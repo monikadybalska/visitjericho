61754234e06dd000af33b13a85aad2e3e540b4ea
Monika <monika.dybalska@hotmail.com>
2024-04-23 16:43:59 +0200

    feat: add homepage loading skeletons

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,25 @@ import PricingPreview from "./ui/homepage/book-a-tour";
 import SeeAndDo from "./ui/homepage/see-and-do";
 import PlanYourTravel from "./ui/homepage/plan-your-travel";
 import MeetThePeople from "./ui/homepage/meet-the-people";
+import ParagraphSkeleton from "./ui/skeletons/paragraph";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <main className="w-full border border-transparent">
       <HomepageHero />
-      <SeeAndDo />
-      <PlanYourTravel />
-      <MeetThePeople />
-      <PricingPreview />
+      <Suspense fallback={<ParagraphSkeleton />}>
+        <SeeAndDo />
+      </Suspense>
+      <Suspense fallback={<ParagraphSkeleton />}>
+        <PlanYourTravel />
+      </Suspense>
+      <Suspense fallback={<ParagraphSkeleton />}>
+        <MeetThePeople />
+      </Suspense>
+      <Suspense fallback={<ParagraphSkeleton />}>
+        <PricingPreview />
+      </Suspense>
     </main>
   );
 }
