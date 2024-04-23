ebb66cb9b8e59f0a119626d8d9d3607506a5c5be
Monika <monika.dybalska@hotmail.com>
2024-04-23 23:10:26 +0200

    feat: polish homepage skeletons

--- app/(homepage)/loading.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default function Loading({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="w-full flex flex-col gap-12 justify-center">{children}</div>
-  );
-}
--- app/ui/homepage/see-and-do.tsx
+++ app/ui/homepage/see-and-do.tsx
@@ -1,21 +1,31 @@
 import CategoryPreview from "./category";
 import SubcategoryPreview from "./subcategory";
 import PlacesPreviews from "./places-previews";
+import { Suspense } from "react";
+import ThreeCardsSkeleton from "../skeletons/three-cards";
 
 export default function SeeAndDo() {
   return (
     <CategoryPreview slug="see-and-do">
       <SubcategoryPreview slug="sights">
-        <PlacesPreviews slug="sights" />
+        <Suspense fallback={<ThreeCardsSkeleton />}>
+          <PlacesPreviews slug="sights" />
+        </Suspense>
       </SubcategoryPreview>
       <SubcategoryPreview slug="nature">
-        <PlacesPreviews slug="nature" />
+        <Suspense fallback={<ThreeCardsSkeleton />}>
+          <PlacesPreviews slug="nature" />
+        </Suspense>
       </SubcategoryPreview>
       <SubcategoryPreview slug="food">
-        <PlacesPreviews slug="food" />
+        <Suspense fallback={<ThreeCardsSkeleton />}>
+          <PlacesPreviews slug="food" />
+        </Suspense>
       </SubcategoryPreview>
       <SubcategoryPreview slug="accommodation">
-        <PlacesPreviews slug="accommodation" />
+        <Suspense fallback={<ThreeCardsSkeleton />}>
+          <PlacesPreviews slug="accommodation" />
+        </Suspense>
       </SubcategoryPreview>
     </CategoryPreview>
   );
