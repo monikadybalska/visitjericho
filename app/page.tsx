34045dcfdee829e3bfec2ee3c9730f64af54287c
Monika <monika.dybalska@hotmail.com>
2024-04-25 17:53:28 +0200

    refactor: move homepage component to root app dir

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import HomepageHero from "../ui/homepage/hero";
-import PricingPreview from "../ui/homepage/book-a-tour";
-import SeeAndDo from "../ui/homepage/see-and-do";
-import PlanYourTravel from "../ui/homepage/plan-your-travel";
+import HomepageHero from "./ui/homepage/hero";
+import PricingPreview from "./ui/homepage/book-a-tour";
+import SeeAndDo from "./ui/homepage/see-and-do";
+import PlanYourTravel from "./ui/homepage/plan-your-travel";
 import { Suspense } from "react";
-import SectionSkeleton from "../ui/skeletons/section";
-import MeetThePeople from "../ui/homepage/meet-the-people";
+import SectionSkeleton from "./ui/skeletons/section";
+import MeetThePeople from "./ui/homepage/meet-the-people";
 
 export default function Home() {
   return (
