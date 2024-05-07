986b35f40e7a0c37cbe8e7d59a395d476253f115
Monika <monika.dybalska@hotmail.com>
2024-05-07 11:03:10 +0200

    feat: optimise images with wordpress sizes

--- app/_components/primitives/cards/card-header.tsx
+++ app/_components/primitives/cards/card-header.tsx
@@ -3,7 +3,6 @@ import { CardHeader as CardHeaderMT } from "../../exports";
 import Image from "next/image";
 
 import { Image as Thumbnail } from "@/app/_lib/types";
-import cloudfrontLoader from "../../../../cloudfrontLoader";
 
 export default function CardHeader({
   thumbnail,
@@ -24,12 +23,14 @@ export default function CardHeader({
             alt=""
             width={fullwidth ? 1248 : 582}
             height={fullwidth ? 429 : 386}
-            className="w-full h-auto"
-            sizes={
-              fullwidth
-                ? "(max-width: 500px) 300px, (max-width: 720px) 500px, (max-width: 960px) 800px, 1248px"
-                : "(max-width: 600px) 400px, (max-width: 720px) 200px, 100px"
-            }
+            className="w-full h-auto md:hidden"
+          />
+          <Image
+            src={thumbnail.node.mediaItemUrl.replace(".jpg", "-384x255.jpg")}
+            alt=""
+            width={fullwidth ? 1248 : 384}
+            height={fullwidth ? 429 : 255}
+            className="w-full h-auto hidden md:block"
           />
         </CardHeaderMT>
       )}
