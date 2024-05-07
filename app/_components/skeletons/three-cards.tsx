5d3e4da255bfb83f692a0c4ca65cbd99f3f1677c
Monika <monika.dybalska@hotmail.com>
2024-05-07 16:24:20 +0200

    fix: fix three cards skeleton layout bug

--- app/_components/skeletons/three-cards.tsx
+++ app/_components/skeletons/three-cards.tsx
@@ -5,7 +5,7 @@ export default function ThreeCardsSkeleton() {
     <div className="w-full max-w-screen-xl gap-4 flex">
       <Skeleton className="h-[400px] w-full rounded-xl basis-full md:basis-1/2 lg:basis-1/3" />
       <Skeleton className="h-[400px] w-full rounded-xl hidden md:flex md:basis-1/2 lg:basis-1/3" />
-      <Skeleton className="h-[400px] w-full rounded-xl hidden lg:basis-1/3" />
+      <Skeleton className="h-[400px] w-full rounded-xl hidden lg:flex lg:basis-1/3" />
     </div>
   );
 }
