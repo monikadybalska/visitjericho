0cae968ec03750bc16261f2312c03d4d993e1a6e
Monika <monika.dybalska@hotmail.com>
2024-05-27 11:03:24 +0200

    feat: make days timeline sticky

--- app/(routes)/itineraries/[itinerary]/_components/timeline-stepper/inner-stepper.tsx
+++ app/(routes)/itineraries/[itinerary]/_components/timeline-stepper/inner-stepper.tsx
@@ -21,49 +21,16 @@ export default function InnerStepper({
     TimelineContext as Context<TimelineStepper>
   );
 
-  const activeLineHeight = useMemo(() => {
-    return completedSteps.size === steps.length
-      ? heightPerStep * (completedSteps.size - 1)
-      : heightPerStep * completedSteps.size;
-  }, [completedSteps, activeDay]);
-
-  useEffect(() => {
-    const heightPerStepCalc = 1000 / (steps.length - 1);
-    setHeightPerStep(heightPerStepCalc);
-  }, [steps, activeDay]);
-
   return (
-    <Stepper
-      className={`flex-col h-[1000px] ${
-        activeDay === index ? "hidden lg:flex" : "hidden"
-      }`}
-      activeStep={activeStep}
-      isLastStep={(value) => setIsLastStep(value)}
-      isFirstStep={(value) => setIsFirstStep(value)}
-      lineClassName={`h-full w-0.5 bg-${color}-light`}
-      activeLineClassName="hidden"
-      style={{ width: "0.125rem" }}
-    >
-      {steps && (
-        <>
-          <div
-            className={`absolute z-0 !w-0.5 bg-green transition-all duration-[3000ms]`}
-            style={{ height: `${Math.round(activeLineHeight)}px` }}
-          ></div>
-          {steps.map((step, j) => (
-            <Step
-              key={j + 1}
-              className={`bg-${color}-light transition-all duration-[3000ms] ${
-                completedSteps.has(j + 1) && `bg-${color}`
-              }`}
-              activeClassName={`bg-${color}`}
-              completedClassName={`bg-${color}`}
-            >
-              <StepCard step={step} index={j + 1} />
-            </Step>
-          ))}
-        </>
-      )}
-    </Stepper>
+    steps &&
+    steps.map((step, j) => (
+      <StepCard
+        key={j}
+        stepsNumber={steps.length}
+        step={step}
+        index={j + 1}
+        dayIndex={index}
+      />
+    ))
   );
 }
--- app/(routes)/itineraries/[itinerary]/_components/timeline-stepper/outer-stepper.tsx
+++ app/(routes)/itineraries/[itinerary]/_components/timeline-stepper/outer-stepper.tsx
@@ -4,6 +4,7 @@ import { useState, useContext, Context } from "react";
 import { Stepper, Step } from "../../../../../_components/exports";
 import { TimelineContext, TimelineStepper } from "./steppers";
 import { cn } from "@/app/_lib/utils";
+import Link from "next/link";
 
 export default function OuterStepper({ className }: { className?: string }) {
   const [isLastDay, setIsLastDay] = useState(false);
@@ -19,31 +20,37 @@ export default function OuterStepper({ className }: { className?: string }) {
   } = useContext(TimelineContext as Context<TimelineStepper>);
 
   return (
-    <Stepper
-      activeStep={activeDay}
-      isLastStep={(value) => setIsLastDay(value)}
-      isFirstStep={(value) => setIsFirstDay(value)}
-      lineClassName={`bg-${color}-light`}
-      activeLineClassName={`bg-${color} max-w-full`}
-      className={cn("", className)}
-    >
-      {days.map((day, i) => (
-        <Step
-          onClick={() => {
-            setActiveDay(i);
-            setActiveStep(0);
-            setCompletedSteps(new Set([]));
-          }}
-          key={i}
-          className={`bg-${color}-light text-[gray]`}
-          activeClassName={`bg-${color} text-black`}
-          completedClassName={`bg-${color} text-black`}
-        >
-          <div className="absolute top-[4.5rem] w-36 text-center">
-            <h3 className={`font-medium font-serif`}>{`Day ${i + 1}`}</h3>
-          </div>
-        </Step>
-      ))}
-    </Stepper>
+    <div className="sticky top-0 z-20 h-[120px] bg-white">
+      <Stepper
+        activeStep={activeDay}
+        isLastStep={(value) => setIsLastDay(value)}
+        isFirstStep={(value) => setIsFirstDay(value)}
+        lineClassName={`bg-${color}-light`}
+        activeLineClassName={`bg-${color} max-w-full`}
+        className={cn("", className)}
+      >
+        {days.map((day, i) => (
+          <Step
+            key={i}
+            onClick={() => {
+              setActiveDay(i);
+            }}
+            className={`bg-${color}-light text-[gray]`}
+            activeClassName={`bg-${color} text-black`}
+            completedClassName={`bg-${color} text-black`}
+          >
+            <Link
+              key={i}
+              href={`#day${i + 1}`}
+              className="absolute w-full min-w-44 h-[120px] z-50 top-0"
+            ></Link>
+            <h3
+              className={`absolute font-medium font-serif top-[4.5rem] w-44 text-center transition-all duration-1000
+                  ${activeDay === i && "text-4xl text-green"}`}
+            >{`Day ${i + 1}`}</h3>
+          </Step>
+        ))}
+      </Stepper>
+    </div>
   );
 }
